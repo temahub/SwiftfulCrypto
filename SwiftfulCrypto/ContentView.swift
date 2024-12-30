280f5fc2581920d62f3dad4f6e8309e45ab4a26b
Jalilov, Artyom <artyom.jalilov@gmail.com>
2024-12-30 23:34:22 +0100

    fixed and test Colors

--- SwiftfulCrypto/ContentView.swift
+++ SwiftfulCrypto/ContentView.swift
@@ -9,13 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+      ZStack {
+        Color.theme.background
+          .ignoresSafeArea()
+        
+        VStack(spacing: 40) {
+          Text("Accent Color")
+            .foregroundStyle(Color.theme.accent)
+          
+          Text("Secondary text color")
+            .foregroundStyle(Color.theme.secondaryText)
+          
+          Text("Red color")
+            .foregroundStyle(Color.theme.red)
+          
+          Text("Green color")
+            .foregroundStyle(Color.theme.green)
         }
-        .padding()
+        .font(.headline)
+      }
     }
 }
 
