4082bb624726c4154d98deeb365d10411582d27a
Jalilov, Artyom <artyom.jalilov@gmail.com>
2024-12-31 21:47:37 +0100

    added and set up CircleButtonView

---
+++ SwiftfulCrypto/Core/Components/CircleButton/CircleButtonView.swift
@@ -0,0 +1,39 @@
+//
+//  CircleButtonView.swift
+//  SwiftfulCrypto
+//
+//  Created by Artyom Jalilov on 31/12/24.
+//
+
+import SwiftUI
+
+struct CircleButtonView: View {
+  
+  let iconName: String
+  
+  var body: some View {
+    Image(systemName: iconName)
+      .font(.headline)
+      .foregroundStyle(Color.theme.accent)
+      .frame(width: 50, height: 50)
+      .background(
+        Circle()
+          .foregroundStyle(Color.theme.background)
+      )
+      .shadow(
+        color: Color.theme.accent.opacity(0.25),
+        radius: 10, x: 0, y: 0)
+      .padding()
+  }
+}
+
+#Preview(traits: .sizeThatFitsLayout) {
+  Group {
+    CircleButtonView(iconName: "info")
+      .padding()
+    
+    CircleButtonView(iconName: "plus")
+      .padding()
+      .colorScheme(.dark)
+  }
+}
