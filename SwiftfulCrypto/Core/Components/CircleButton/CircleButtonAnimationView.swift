4906797f33cf95f4a9b772b0de1195025d53d9db
Jalilov, Artyom <artyom.jalilov@gmail.com>
2025-01-01 21:17:04 +0100

    added and implemented CircleButtonAnimationView

---
+++ SwiftfulCrypto/Core/Components/CircleButton/CircleButtonAnimationView.swift
@@ -0,0 +1,27 @@
+//
+//  CircleButtonAnimationView.swift
+//  SwiftfulCrypto
+//
+//  Created by Artyom Jalilov on 1/1/25.
+//
+
+import SwiftUI
+
+struct CircleButtonAnimationView: View {
+  
+  @Binding var animate: Bool
+  
+  var body: some View {
+    Circle()
+      .stroke(lineWidth: 5.0)
+      .scale(animate ? 1.0 : 0.0)
+      .opacity(animate ? 0.0 : 1.0)
+      .animation(animate ? .easeOut(duration: 1.0) : .none, value: animate)
+  }
+}
+
+#Preview {
+  CircleButtonAnimationView(animate: .constant(false))
+    .foregroundStyle(.red)
+    .frame(width: 100, height: 100)
+}
--- SwiftfulCrypto/Core/Home/Views/HomeView.swift
+++ SwiftfulCrypto/Core/Home/Views/HomeView.swift
@@ -19,30 +19,39 @@ struct HomeView: View {
       
       // content layer
       VStack {
-        HStack {
-          CircleButtonView(iconName: showPortfolio ? "plus" : "info")
-            .animation(.none, value: 0)
-          Spacer()
-          
-          Text(showPortfolio ? "Portfolio" :"Live prices")
-            .font(.headline)
-            .fontWeight(.heavy)
-            .foregroundStyle(Color.theme.accent)
-            .animation(.none, value: 0)
-          
-          Spacer()
-          CircleButtonView(iconName: "chevron.right")
-            .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
-            .onTapGesture {
-              withAnimation(.spring()) {
-                showPortfolio.toggle()
-              }
-            }
-        }
-        .padding(.horizontal)
+        homeHeader
         Spacer(minLength: 0)
       }
     }
+  }
+}
+
+extension HomeView {
+  private var homeHeader: some View {
+    HStack {
+      CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+        .animation(.none, value: false)
+        .background(
+          CircleButtonAnimationView(animate: $showPortfolio)
+        )
+      Spacer()
+      
+      Text(showPortfolio ? "Portfolio" :"Live prices")
+        .font(.headline)
+        .fontWeight(.heavy)
+        .foregroundStyle(Color.theme.accent)
+        .animation(.none, value: false)
+      
+      Spacer()
+      CircleButtonView(iconName: "chevron.right")
+        .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+        .onTapGesture {
+          withAnimation(.spring()) {
+            showPortfolio.toggle()
+          }
+        }
+    }
+    .padding(.horizontal)
   }
 }
 
