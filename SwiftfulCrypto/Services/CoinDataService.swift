5305efe5654b0e1eff671e06a494363865a031d4
Jalilov, Artyom <artyom.jalilov@gmail.com>
2025-01-04 17:17:07 +0100

    added and set up CoinDataService for downloading coins and creating [CoinModel]. Subscribe on it in HomeViewModel to download data and show it on main screen

--- SwiftfulCrypto/Services/CoinDataService.swift
+++ SwiftfulCrypto/Services/CoinDataService.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+import Combine
+
+class CoinDataService {
+  @Published var allCoins: [CoinModel] = []
+//  var cancellables = Set<AnyCancellable>()
+  var coinSubscription: AnyCancellable?
+  
+  init() {
+    getCoins()
+  }
+  
+  private func getCoins() {
+    guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h")
+    else { return }
+    
+    // TODO: Replace Combine to Structured Concurrency
+    coinSubscription = URLSession.shared.dataTaskPublisher(for: url)
+      .subscribe(on: DispatchQueue.global(qos: .default))
+      .tryMap { (output) -> Data in
+        guard let response = output.response as? HTTPURLResponse,
+              response.statusCode >= 200 && response.statusCode < 300
+        else {
+          throw URLError(.badServerResponse)
+        }
+        return output.data
+      }
+      .receive(on: DispatchQueue.main)
+      .decode(type: [CoinModel].self, decoder: JSONDecoder())
+      .sink { (completion) in
+        switch completion {
+        case .finished:
+          break
+        case .failure(let error):
+          print(".sink error: \(error.localizedDescription)")
+        }
+      } receiveValue: { [weak self] (returnedCoins) in
+        self?.allCoins = returnedCoins
+        self?.coinSubscription?.cancel()
+      }
+//      .store(in: &cancellables)
+
+  }
+}
