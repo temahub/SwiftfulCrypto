234b5f6a52b85286ab998e0d2dca9d91bdab898a
Jalilov, Artyom <artyom.jalilov@gmail.com>
2025-01-07 00:25:41 +0100

    setup in HomeVIewModel coins search using Search Bar

--- SwiftfulCrypto/Core/Home/ViewModels/HomeViewModel.swift
+++ SwiftfulCrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -22,10 +22,33 @@ class HomeViewModel: ObservableObject {
   
   func addSubscribers() {
     // TODO: Replace Combine to Structured Concurrency
-    dataService.$allCoins
+//    dataService.$allCoins
+//      .sink { [weak self] (returnedCoins) in
+//        self?.allCoins = returnedCoins
+//      }
+//      .store(in: &cancellables)
+    
+    // updates allCoins
+    $searchText
+      .combineLatest(dataService.$allCoins)
+      .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+      .map(filterCoins)
       .sink { [weak self] (returnedCoins) in
         self?.allCoins = returnedCoins
       }
       .store(in: &cancellables)
+  }
+  
+  private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
+    guard !text.isEmpty else {
+      return coins
+    }
+    
+    let lowercasedText  = text.lowercased()
+    return coins.filter { (coin) in
+      return coin.name.lowercased().contains(lowercasedText) ||
+      coin.symbol.lowercased().contains(lowercasedText) ||
+      coin.id.lowercased().contains(lowercasedText)
+    }
   }
 }
